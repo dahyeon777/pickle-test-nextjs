bc6cca502259e4efd96f19040934f7810c3b9fa0
dahyeon777 <daaaa0727@gmail.com>
2026-01-05 15:06:45 +0900

    feat: 호러 테스트셋 11~17 추가

--- src/HorrorTestData/index.js
+++ src/HorrorTestData/index.js
@@ -8,19 +8,33 @@ import { horrorTestData7 } from "./testData7";
 import { horrorTestData8 } from "./testData8";
 import { horrorTestData9 } from "./testData9";
 import { horrorTestData10 } from "./testData10";
+import { horrorTestData11 } from "./testData11";
+import { horrorTestData12 } from "./testData12";
+import { horrorTestData13 } from "./testData13";
+import { horrorTestData14 } from "./testData14";
+import { horrorTestData15 } from "./testData15";
+import { horrorTestData16 } from "./testData16";
+import { horrorTestData17 } from "./testData17";
 
 // 1. 모든 원본 데이터를 담은 리스트
 export const AllHorrorQuestionsData = [
-  horrorTestData1,
-  horrorTestData2,
-  horrorTestData3,
-  horrorTestData4,
-  horrorTestData5,
-  horrorTestData6,
-  horrorTestData7,
-  horrorTestData8,
-  horrorTestData9,
-  horrorTestData10,
+  horrorTestData1, //대기표
+  horrorTestData15, //장화홍련
+  horrorTestData10, //야생동물
+  horrorTestData2, //저수지
+  horrorTestData13, //학교
+  horrorTestData14, //병원
+  horrorTestData16, //심청이
+  horrorTestData5, //세차
+  horrorTestData11, //엘리베이터
+  horrorTestData12, //편의점
+  horrorTestData3, //대답먼저1
+  horrorTestData4, //대답먼저2
+  horrorTestData7, //인수인계
+  horrorTestData8, //계절 사이비
+  horrorTestData9, //등대
+  horrorTestData6, //반사
+  horrorTestData17, //헨젤과 그레텔
 ];
 
 // 2. testDataList가 원본 객체 전체를 참조하도록 수정 (가장 중요!)
